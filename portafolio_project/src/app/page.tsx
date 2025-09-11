fce4e3c840c3cc9fcda7c913d829917b5bf120ce
Juan Camilo Maerínez Bedoya <juan.martinezbed@campusucc.edu.co>
2025-09-10 22:52:22 -0500

    Proyecto progreso portafolio

---
+++ portafolio_project/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-black text-2xl md:text-4xl lg:text-4xl text-center">
+        Juan Camilo Martinez Bedoya
+      </h1>
+    </main>
+  );
+}
